9a8270d18442f7f14351ca923a0e5272f8efa649
Basia <barbara.borodziewicz@gmail.com>
2019-05-24 11:04:25 -0500

    trying to get api response

--- src/WeatherMain.js
+++ src/WeatherMain.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import "./WeatherMain.css";
+import Axios from "axios";
+import Api from "./Api";
 
 export default class WeatherMain extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    city: "current location",
+    unit: "metric",
+    date: "current date"
+  }
+}
   render() {
+    Axios.get(<Api />).then(response => {
+      return {
+name: response.data.name,
+      }
+    })
     return (
       <div>
         <div className="weather-main-heading">
-          <h1 className="weather-main-city">City</h1>
+          <h1 className="weather-main-city">{this.state.city}</h1>
           <p className="weather-main-date">Day time</p>
           <p className="weather-main-description">Description</p>
         </div>
