b4889a4fa50fa1f7d5a2a16d4b78988de841df94
Basia <barbara.borodziewicz@gmail.com>
2019-05-24 09:36:00 -0500

    fixed api

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Api extends Component {
   render() {
-    let url = "http://samples.openweathermap.org/data/2.5";
+    let url = "api.openweathermap.org/data/2.5";
     let path = "weather";
     let apiKey = "ace2c200d6c61096c76082a9e2846e29";
     let city = "Lisbon";
