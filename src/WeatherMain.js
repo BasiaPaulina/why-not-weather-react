7f467fb5ead01c9986b067386d6d47f2c915b7fc
Basia <barbara.borodziewicz@gmail.com>
2019-05-24 14:36:49 -0500

    fixed api call

--- src/WeatherMain.js
+++ src/WeatherMain.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
 import "./WeatherMain.css";
-import Axios from "axios";
+import axios from "axios";
 import Api from "./Api";
 
 export default class WeatherMain extends Component {
 constructor(props){
   super(props);
   this.state = {
-    city: "current location",
-    unit: "metric",
-    date: "current date"
-  }
+    loaded: false,
+  };
+
+  let root = "https://api.openweathermap.org/data/2.5";
+    let path = "weather";
+    let apiKey = "ace2c200d6c61096c76082a9e2846e29";
+    let city = "Lisbon";
+    let units = "metric";
+    let paths = `?q=${city}&appid=${apiKey}&units=${units}`;
+    let url = `${root}/${path}${paths}`;
+  axios.get(url).then((response) => {
+console.log(response);
+  });
 }
+
   render() {
-    Axios.get(<Api />).then(response => {
-      return {
-name: response.data.name,
-      }
-    })
+   if (this.state.loaded) {
     return (
       <div>
         <div className="weather-main-heading">
-          <h1 className="weather-main-city">{this.state.city}</h1>
+          <h1 className="weather-main-city">City</h1>
           <p className="weather-main-date">Day time</p>
           <p className="weather-main-description">Description</p>
         </div>
@@ -34,5 +40,12 @@ name: response.data.name,
         </ul>
       </div>
     );
+   } else {
+     return(<div>
+      <h1 className="weather-main-city">Weather is Loading</h1>
+    </div>);
+     
+   }
+  
   }
 }
